09b1cf5a30f54864a33d15f5a3d4a3881e0994b3
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-03 20:38:55 +0000

    [Fix] Execute at the puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,6 +1,15 @@
-# Install the response
+# Update all the O.S
+# Do the response
 
-exec {'http_res':
-  path    => '/bin',
-  command => 'sudo apt-get -y upgrade && sudo apt-get -y update && sudo apt-get -y install nginx && sudo sed -i "11i\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf && sudo service nginx restart;'
+exec {'Update':
+  path     => '/bin',
+  provider => shell,
+  command  => 'sudo apt-get -y upgrade; sudo apt-get -y update; sudo apt-get -y install nginx; echo "Holberton School" | sudo tee /var/www/html/index.html',
+  before   => Exec['res'],
+}
+
+exec {'res':}
+  path     => '/bin',
+  provider => shell,
+  command  => 'sudo sed -i "11i\\\tadd_header X-Served-By ${hostname};" /etc/nginx/nginx.conf; sudo service nginx restart;',
 }
