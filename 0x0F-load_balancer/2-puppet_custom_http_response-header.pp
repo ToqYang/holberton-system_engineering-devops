02f6f6bbe25347669e6c2456943806987cb32a3a
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-03 19:37:35 +0000

    [Fix] fix cut lines

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -2,6 +2,5 @@
 
 exec {'http_res':
   path    => '/bin',
-  command => 'sudo apt-get -y upgrade && sudo apt-get -y update && sudo apt-get -y install nginx &&\
-  	     sudo sed -i "11i\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf && sudo service nginx restart;'
+  command => 'sudo apt-get -y upgrade && sudo apt-get -y update && sudo apt-get -y install nginx && sudo sed -i "11i\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf && sudo service nginx restart;'
 }
