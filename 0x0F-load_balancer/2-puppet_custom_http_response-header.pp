75a32bebdde40fbac99df20a5410b1294cd4d5d5
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-03 21:09:58 +0000

    [Fix] Delete upgrade

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -3,11 +3,12 @@
 
 exec {'Update':
   provider => shell,
-  command  => 'sudo apt-get -y upgrade; sudo apt-get -y update; sudo apt-get -y install nginx; echo "Holberton School" | sudo tee /var/www/html/index.html',
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Holberton School" | sudo tee /var/www/html/index.html',
   before   => Exec['res'],
 }
 
+
 exec {'res':
   provider => shell,
-  command  => 'sudo sed -i "11i\\\tadd_header X-Served-By ${hostname};" /etc/nginx/nginx.conf; sudo service nginx restart;',
+  command  => 'sudo sed -i "11i\\\tadd_header X-Served-By $hostname;" /etc/nginx/nginx.conf; sudo service nginx restart;',
 }
