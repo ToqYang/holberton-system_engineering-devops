89776d27b94f6a6a9e054bac567fddba3207a666
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-03 19:34:14 +0000

    [Fix] Https response

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,7 @@
+# Install the response
+
+exec {'http_res':
+  path    => '/bin',
+  command => 'sudo apt-get -y upgrade && sudo apt-get -y update && sudo apt-get -y install nginx &&\
+  	     sudo sed -i "11i\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf && sudo service nginx restart;'
+}
