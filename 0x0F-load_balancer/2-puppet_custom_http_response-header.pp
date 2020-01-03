cde4dbf53a1f26de80ca7d72c1723f1f74f6f279
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-03 20:45:08 +0000

    [Fix] Delete the path

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -2,14 +2,12 @@
 # Do the response
 
 exec {'Update':
-  path     => '/usr/bin',
   provider => shell,
   command  => 'sudo apt-get -y upgrade; sudo apt-get -y update; sudo apt-get -y install nginx; echo "Holberton School" | sudo tee /var/www/html/index.html',
   before   => Exec['res'],
 }
 
 exec {'res':}
-  path     => '/usr/bin',
   provider => shell,
   command  => 'sudo sed -i "11i\\\tadd_header X-Served-By ${hostname};" /etc/nginx/nginx.conf; sudo service nginx restart;',
 }
