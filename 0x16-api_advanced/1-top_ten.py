e2f3a0c24076f7255fe792b5ed658177f6fe80c2
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-06 22:08:45 +0000

    [Feature] Top ten hotlist

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    10 more host-post
+
+    Args:
+        subreddit: Name of the forum of reddit
+
+    Returns:
+        (int) Number of hot list
+        0 if the subreddit is incorrect
+    """
+    base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    req = requests.get(base_url,
+                       headers={'user-agent': 'Changuas'},
+                       allow_redirects=False)
+
+    if (req.status_code != 200):
+        return (0)
+
+    num = req.json().get('data').get('children')
+    count = 0
+
+    for elem in num:
+        print(elem.get('data').get('title'))
+        count += 1
+        if (count == 10):
+            break
