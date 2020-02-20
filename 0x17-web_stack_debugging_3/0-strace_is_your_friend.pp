d93682e0235f5708fb41fcbfe03476193f357c13
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-19 21:06:28 -0500

    [Feature] Add comments and put spaces

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,7 @@
-exec { 'change-name':                         
-path     => 'usr/bin/:/bin/',                         
-command  => "sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",                         
-provider => 'shell',                       
+# Change the name .phpp to php
+
+exec { 'change-name':
+  path     => 'usr/bin/:/bin/',
+  command  => "sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  provider => 'shell',
 }
