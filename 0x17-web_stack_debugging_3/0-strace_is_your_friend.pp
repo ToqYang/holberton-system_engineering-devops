c55e2417bf4142dd5d19c05202262d11136b5245
Santiago Yanguas <santitoya-2001@outlook.com>
2020-02-19 20:46:16 -0500

    [Feature] Change the name of the files

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+exec { 'fix-phpp':                         
+path     => 'usr/bin/:/bin/',                         
+command  => "sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",                         
+provider => 'shell',                       
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+Advance abot detect problems
