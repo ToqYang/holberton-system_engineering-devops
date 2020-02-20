43838d83836cb97ffaaa10feb87787726ca6a4cf
Santiago <45366195+ToqYang@users.noreply.github.com>
2020-02-19 20:49:30 -0500

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-exec { 'fix-phpp':                         
+exec { 'change-name':                         
 path     => 'usr/bin/:/bin/',                         
 command  => "sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",                         
 provider => 'shell',                       
