89484d20b35d631562ffd07ba0361edd60f121b9
Santiago <45366195+ToqYang@users.noreply.github.com>
2020-02-27 20:54:43 -0500

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,2 +1,17 @@
-# Execute change to the hard and soft limit                                                                                                                                                                                                                                                                                                             exec {'change-hard':                                                                                                                                                          provider => shell,                                                                                                                                                          command  => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 5000/" /etc/security/limits.conf',                                                                before   => Exec['res'],                                                                                                                                                  }                                                                                                                                                                                                                                                                                                                                                       exec {'change-soft':                                                                                                                                                          provider => shell,                                                                                                                                                          command  => 'sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 4000/" /etc/security/limits.conf',                                                                before   => Exec['upd-nginx'],                                                                                                                                            }                                                                                                                                                                                                                                                                                                                                                       exec {'upd-nginx':                                                                                                                                                            provider => shell,                                                                                                                                                          command  => 'sudo service nginx restart;',
+# Execute change to the hard and soft limit
+
+exec {'change-hard':
+  provider => shell,
+  command  => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 5000/" /etc/security/limits.conf',
+  before   => Exec['res'],
+}
+
+exec {'change-soft':
+  provider => shell,
+  command  => 'sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 4000/" /etc/security/limits.conf',
+  before   => Exec['upd-nginx'],
+}
+exec {'upd-nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart;',
 }
